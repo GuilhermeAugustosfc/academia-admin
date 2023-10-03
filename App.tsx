2e90f8b381f5b28a8e9feaacba06ac7a8f236a01
Guilherme Augusto <guilherme_augustosfc@hotmail.com>
2023-10-03 19:57:53 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,41 +1,127 @@
-import React from 'react'
-import { View, FlatList, StatusBar, Text } from 'react-native'
-import { INITIAL_DATA } from './data'
-import { styles } from './styles'
-import { ListItem } from './src/components/ListItem'
-import { getMonth } from './src/components/util'
-import { Menu } from './src/components/Menu'
+import { useState, useEffect, useRef } from 'react'
+import { Text, View, Button, Platform } from 'react-native'
+import * as Device from 'expo-device'
+import * as Notifications from 'expo-notifications'
 
-export type Item = {
-  id: string
-  name: string
-  date_cadastro: string
-  payment: boolean
-}
+import Constants from 'expo-constants'
 
-const statusBarHeight = StatusBar.currentHeight || 0
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+})
 
-const App = () => {
-  // Ordena a array 'pessoas' com base na chave 'idade'
-  INITIAL_DATA.sort((itemA, itemB) => {
-    if (!itemA.payment && itemB.payment) {
-      return -1 // itemA vem antes de itemB
-    } else if (itemA.payment && !itemB.payment) {
-      return 1 // itemB vem antes de itemA
-    } else {
-      return 0 // Mantém a ordem original
+export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState<string>()
+  const [notification, setNotification] = useState<Notifications.Notification>()
+  const notificationListener = useRef<Notifications.Subscription>()
+  const responseListener = useRef<Notifications.Subscription | null>()
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      setExpoPushToken(token)
+    })
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification)
+      })
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response)
+      })
+
+    return () => {
+      if (notificationListener.current && responseListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        )
+        Notifications.removeNotificationSubscription(responseListener.current)
+      }
     }
-  })
+  }, [])
+
   return (
-    <View style={{ marginTop: statusBarHeight }}>
-      <Menu />
-      <FlatList
-        data={INITIAL_DATA}
-        renderItem={({ item }) => <ListItem item={item} />}
-        keyExtractor={(item) => item.id}
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+      }}
+    >
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Text>
+          Title: {notification && notification.request.content.title}{' '}
+        </Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
+        <Text>
+          Data:{' '}
+          {notification && JSON.stringify(notification.request.content.data)}
+        </Text>
+      </View>
+      <Button
+        title="Press to schedule a notification"
+        onPress={async () => {
+          await schedulePushNotification()
+        }}
       />
     </View>
   )
 }
 
-export default App
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: 'Você realizou uma nova venda! 💸',
+      body: 'Nova venda realizada no valor de R$40,00',
+      data: { data: 'goes here' },
+      sound: 'notification_sound.wav',
+      vibrate: [0, 250, 250, 250],
+    },
+    trigger: { seconds: 2 },
+  })
+}
+
+async function registerForPushNotificationsAsync() {
+  let token
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+      sound: 'notification_sound.wav',
+    })
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync()
+    let finalStatus = existingStatus
+
+    if (existingStatus !== 'granted') {
+      const response = await Notifications.requestPermissionsAsync()
+      let status = response.status
+      finalStatus = status
+    }
+
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!')
+      return
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    token = (
+      await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig?.extra?.eas.projectId,
+      })
+    ).data
+  } else {
+    alert('Must use physical device for Push Notifications')
+  }
+
+  return token
+}
